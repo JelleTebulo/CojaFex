bbb397d11979288a1db2bd97a45c332dbb88b926
JelleTebulo <jelle.kelbling@tebulo.com>
2022-07-11 16:04:42 +0200

    Intermediary push 2 Working on plotting data

--- Plot_RobotDATA.py
+++ Plot_RobotDATA.py
@@ -49,15 +49,51 @@
 #
 #         pnewCalc := [rZ_Calc,rY_Calc,rX_Calc];
 
+
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib.animation import FuncAnimation
 import pandas as pd
 
-data = np.array(pd.read_csv('Data/TargetNewData_11_07_22.csv'))
+import math
 
-# fig, (ax) = plt.subplots(subplot_kw=dict(projection="3d"))
+def euler_yzx_to_axis_angle(z_e, x_e, y_e, normalize=True):
+    # Assuming the angles are in radians.
+    c1 = math.cos(z_e/2)
+    s1 = math.sin(z_e/2)
+    c2 = math.cos(x_e/2)
+    s2 = math.sin(x_e/2)
+    c3 = math.cos(y_e/2)
+    s3 = math.sin(y_e/2)
+    c1c2 = c1*c2
+    s1s2 = s1*s2
+    w = c1c2*c3 - s1s2*s3
+    x = c1c2*s3 + s1s2*c3
+    y = s1*c2*c3 + c1*s2*s3
+    z = c1*s2*c3 - s1*c2*s3
+    angle = 2 * math.acos(w)
+    if normalize:
+        norm = x*x+y*y+z*z
+        if norm < 0.001:
+            # when all euler angles are zero angle =0 so
+            # we can set axis to anything to avoid divide by zero
+            x = 1
+            y = 0
+            z = 0
+        else:
+            norm = math.sqrt(norm)
+            x /= norm
+            y /= norm
+            z /= norm
+    return z, x, y, angle
+
+
+data = np.array(pd.read_csv('Data/TargetNew_11-7-22-1216.csv'))
+data = np.array(pd.read_csv('Data/TargetNew_11-7-22-1154.csv'))
+data = np.array(pd.read_csv('Data/TargetNew_11-7-22-1326.csv'))
+
+
 x = data[:, 0]
 y = data[:, 1]
 z = data[:, 2]
@@ -68,6 +104,7 @@ u = u1 / np.linalg.norm([u1, v1, w1], axis=0)
 v = v1 / np.linalg.norm([u1, v1, w1], axis=0)
 w = w1 / np.linalg.norm([u1, v1, w1], axis=0)
 
+# fig, (ax) = plt.subplots(subplot_kw=dict(projection="3d"))
 # ax.plot(x, y, z, lw=1, c='k', linestyle = '--')
 #
 # fig2, ax2 = plt.subplots()
@@ -79,29 +116,40 @@ w = w1 / np.linalg.norm([u1, v1, w1], axis=0)
 # ax2.plot(w, lw=1, c='k', linestyle = ':')
 # ax2.plot(np.linalg.norm([u,v,w], axis=0), lw=1, c='g', linestyle = '-')
 
+axis_angle = np.zeros(np.shape(data)[0])
+from scipy.spatial.transform import Rotation as R
 
-fig3, axs3 = plt.subplots(2, 3, sharex=True)
-end = 291
-rx = data[:end, 3]
-ry = data[:end, 4]
-rz = data[:end, 5]
-axs3[0, 0].plot(rx, c='b', linestyle='-')
-axs3[0, 1].plot(ry, c='c', linestyle='-')
-axs3[0, 2].plot(rz, c='k', linestyle='-')
-drx = np.diff(data[:end, 3])
-dry = np.diff(data[:end, 4])
-drz = np.diff(data[:end, 5])
-axs3[1, 0].plot(drx, c='b', linestyle='-')
-axs3[1, 1].plot(dry, c='c', linestyle='-')
-axs3[1, 2].plot(drz, c='k', linestyle='-')
+rx = data[:, 3]
+ry = data[:, 4]
+rz = data[:, 5]
+drx = np.diff(data[:, 3])
+dry = np.diff(data[:, 4])
+drz = np.diff(data[:, 5])
+for i in range(len(drx)):
+    z, x, y, angle = euler_yzx_to_axis_angle(drz[i], drx[i], dry[i], normalize=True)
+    axis_angle[i] = angle
+
+beg, end = 0, 1300
+FS = 15
+fig3, axs3 = plt.subplots(2, 4, sharex=True)
+axs3[0, 0].plot(rx[beg:end], c='b', linestyle='-')
+axs3[0, 1].plot(ry[beg:end], c='c', linestyle='-')
+axs3[0, 2].plot(rz[beg:end], c='k', linestyle='-')
+axs3[0, 0].set_xlabel('$rx$', fontsize=FS)
+axs3[0, 1].set_xlabel('$ry$', fontsize=FS)
+axs3[0, 2].set_xlabel('$rz$', fontsize=FS)
+axs3[1, 0].plot(drx[beg:end], c='b', linestyle='-')
+axs3[1, 1].plot(dry[beg:end], c='c', linestyle='-')
+axs3[1, 2].plot(drz[beg:end], c='k', linestyle='-')
+axs3[1, 0].set_xlabel('$drx$', fontsize=FS)
+axs3[1, 1].set_xlabel('$dry$', fontsize=FS)
+axs3[1, 2].set_xlabel('$drz$', fontsize=FS)
+
+axs3[1, 3].plot(axis_angle[beg:end], c='k', linestyle='-')
+axs3[1, 3].set_xlabel('$d axis angle$', fontsize=FS)
+axs3[0, 3].plot(np.diff(data[beg:end, 3]), c='b', linestyle='-')
+axs3[0, 3].plot(np.diff(data[beg:end, 4]), c='c', linestyle='-')
+axs3[0, 3].plot(np.diff(data[beg:end, 5]), c='k', linestyle='-')
+axs3[0, 3].set_xlabel('$rx, ry, rz$', fontsize=FS)
 
 
-# for th in range(0, 180, 20):
-#     th = np.deg2rad(th)
-#     a = np.array([np.cos(th),np.sin(th),0])
-#     b = np.array([-np.sin(th),np.cos(th),0])
-#     a_s = a/np.linalg.norm(a)
-#     b_s = b/np.linalg.norm(b)
-#     c = np.cross(a_s, b_s, axis=0)
-#     c_s = c/ np.linalg.norm(c)
-#     print(f'a =', np.linalg.norm(a_s), '          b =', np.linalg.norm(b_s), '          c =', np.linalg.norm(c))
